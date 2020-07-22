8d0b87075b3fc30a5086e1dd72eb3b16849a6c7c
Roman Pavlenko <rpavlenko.mail@gmail.com>
2020-07-22 17:59:02 +0200

    refactor: remove unused styles

--- src/modules/selectClub.js
+++ src/modules/selectClub.js
@@ -1,20 +1,19 @@
 const selectClub = () => {
   const club = document.querySelector('.club-list');
 
+  let opacity = 0;
+  const changeOpacity = elem => {
+    if (opacity < 1) {
+      opacity += 0.1;
+      setTimeout(() => {
+        changeOpacity(elem);
+      }, 10);
+    }
+    elem.style.opacity = opacity;
+  };
+
   document.addEventListener('click', event => {
     let target = event.target;
-    club.style.opacity = '-0.1';
-    let opacity = 0;
-
-    const changeOpacity = elem => {
-      if (opacity < 1) {
-        opacity += 0.1;
-        setTimeout(() => {
-          changeOpacity(elem);
-        }, 10);
-      }
-      elem.style.opacity = opacity;
-    };
 
     if (target.closest('.clubs-list')) {
       club.style.display = 'block';
@@ -23,7 +22,6 @@ const selectClub = () => {
       target = target.closest('.clubs-list');
       if (!target) {
         club.style.display = 'none';
-        club.style.transition = 'opacity 0.1s';
       }
     }
   });
