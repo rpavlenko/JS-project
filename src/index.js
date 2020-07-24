bca6b6f42a4a5a17f04e6e56d6f6e89792c43011
Roman Pavlenko <rpavlenko.mail@gmail.com>
2020-07-24 19:53:38 +0200

    feat: add sendForm module, maskPhone, preloader styles css & rebuilt main.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 // eslint-disable-next-line strict
 'use strict';
 
+const freeVisitPopUp = document.getElementById('free_visit_form'),
+  callbackForm = document.getElementById('callback_form'),
+  freeVisitPopUpButton = document.querySelector('.open-popup'),
+  callbackFormpButton = document.querySelector('.callback-btn');
+
+
 import 'nodelist-foreach-polyfill';
 import '@babel/polyfill';
 import 'formdata-polyfill';
@@ -13,15 +19,24 @@ import smoothscroll from 'smoothscroll-polyfill';
 smoothscroll.polyfill();
 
 import selectClub from './modules/selectClub';
-import freeVisitPopUp from './modules/freeVisitPopUp';
-import callbackForm from './modules/callbackForm';
+import modalOpen from './modules/modalOpen';
 import showGift from './modules/showGift';
 import sliders from './modules/sliders';
+import sendForm from './modules/sendForm';
+import maskPhone from './modules/maskPhone';
 
 selectClub();
-freeVisitPopUp();
-callbackForm();
+modalOpen(freeVisitPopUp, freeVisitPopUpButton);
+modalOpen(callbackForm, callbackFormpButton);
 showGift();
 sliders();
+maskPhone('#phone');
+maskPhone('#callback_form-phone');
+maskPhone('#callback_form1-phone');
+maskPhone('#callback_form2-phone');
+maskPhone('#callback_footer_form-phone');
+sendForm();
+
+
 
 
---
+++ src/modules/sendForm.js
@@ -0,0 +1,117 @@
+const sendForm = () => {
+  const errorMessage = 'Что-то пошло не так...',
+    statusMessage = document.createElement('div'),
+    successPopUp = document.getElementById('thanks'),
+    successPopUpContent = document.querySelector('.form-thanks-message'),
+    thanksMessageTitle = document.querySelector('.thanks-message-title'),
+    elementsForm = [];
+
+  statusMessage.style.cssText = 'font-size: 1rem; color: #ffd11a;';
+  const loadMessage = `<section class="sk-chasing-dots">
+    <div class= "sk-child sk-dot-1"> </div>
+    <div class= "sk-child sk-dot-2"> </div>
+    </section>`;
+
+  const valid = () => {
+    elementsForm.forEach(elem => {
+      const pattern = /[А-Яа-яЁе ]/;
+
+      elem.value = elem.value.replace(/[^А-Яа-яЁе ]/, '');
+      if (!pattern.test(elem.value)) {
+        elem.value = '';
+        elem.style.border = '2px solid tomato';
+      } else {
+        elem.style.border = '';
+      }
+    });
+  };
+
+  document.addEventListener('input', event => {
+    const target = event.target;
+    if (target.matches('input') && target.closest('form') && !target.matches('.promo')) {
+      if (target.tagName !== 'button' && target.type !== 'button' && target.type === 'text') {
+        elementsForm.push(target);
+        valid();
+      }
+    }
+  });
+
+  const postData = body => fetch('./server.php', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  });
+
+  document.addEventListener('submit', event => {
+    let target = event.target;
+    const checkbox = target.querySelector('input[type = "checkbox"]'),
+      personalData = target.querySelector('.personal-data');
+
+    event.preventDefault();
+
+    if (checkbox) {
+      if (!checkbox.checked) {
+        personalData.appendChild(statusMessage);
+        statusMessage.textContent = 'Необходимо подтвердить согласие!';
+        statusMessage.style.padding = "2px 0";
+        return;
+      }
+    }
+
+    target.appendChild(statusMessage);
+    statusMessage.innerHTML = loadMessage;
+
+    const formData = new FormData(target);
+    const body = {};
+
+    formData.forEach((val, key) => {
+      body[key] = val;
+    });
+
+    const checkIfPopUpExists = () => {
+      if (target.closest('.popup')) {
+        target = target.closest('.popup');
+        target.style.display = 'none';
+      }
+    };
+
+    postData(body)
+      .then(response => {
+        if (response.status !== 200) {
+          throw new Error('status not 200');
+        }
+        checkIfPopUpExists();
+        successPopUp.style.display = 'block';
+        statusMessage.textContent = '';
+      })
+      .catch(error => {
+        checkIfPopUpExists();
+        successPopUp.style.display = 'block';
+        successPopUpContent.textContent = '';
+        thanksMessageTitle.textContent = 'Ой!';
+        successPopUpContent.appendChild(statusMessage);
+        statusMessage.textContent = errorMessage;
+        console.error(error);
+      });
+
+    successPopUp.addEventListener('click', event => {
+      let target = event.target;
+      if (target.classList.contains('close_icon') || target.classList.contains('close-btn')) {
+        successPopUp.style.display = 'none';
+        successPopUp.style.opacity = '';
+      } else {
+        target = target.closest('.form-content');
+        if (!target) {
+          successPopUp.style.display = '';
+          successPopUp.style.opacity = '';
+        }
+      }
+    });
+
+    target.reset();
+  });
+};
+
+export default sendForm;
